328ef4e64de28e84d9a1cf98eaaef94d4d935ed4
Sefat Anam <sefatanam@gmail.com>
2020-05-27 23:02:37 +0600

    Member Object added

--- screens/AppHome/Home.js
+++ screens/AppHome/Home.js
@@ -2,10 +2,28 @@ import React from 'react';
 import {Text, View, StyleSheet, Alert} from 'react-native';
 import {Button} from 'react-native-paper';
 
+class Member {
+  id = '';
+  name = '';
+  phone = '';
+  email = '';
+}
+
 const Home = props => {
+
+  let obj = new Member();
+  obj.name = 'Sefat Anam';
+  obj.phone = '4654648';
+  obj.email = 'someone@email.com';
+  obj.id = '1';
+
   const iconButtonClicked = () => {
-    Alert.alert('Icon Button Clicked');
+    Alert.alert(`From Home.js ${obj.name} ${obj.email}\n ${obj.phone}`);
   };
+
+
+
+
   return (
     <View style={styles.container}>
       <Text>Home</Text>
